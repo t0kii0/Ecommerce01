00df9b25b5dedf00de9ae2a0dad3e80d8562e143
t0kii0 <ang.sepulvedao@duocuc.cl>
2025-05-05 22:15:42 -0400

    prueba

--- src/pages/CellPhonePage.tsx
+++ src/pages/CellPhonePage.tsx
@@ -1,34 +1,34 @@
 import { CardProduct } from "../components/products/CardProduct";
 import { ContainerFilter } from "../components/products/ContainerFilter";
-import { allCelulares } from "../data/initialData"
+import { allCelulares } from "../data/initialData";
 import { prepareProducts } from "../helpers";
 
 export const CellPhonePage = () => {
+  const preparedProducts = prepareProducts(allCelulares);
 
-        const preparedProducts = prepareProducts(allCelulares);
+  return (
+    <>
+      <h1 className="text-5xl font-semibold text-center mb-12">Celulares</h1>
 
-    return <>
-    <h1 className="text-5xl font-semibold text-center mb-12">
-        Celulares
-    </h1>
-
-    <div className="grid grid-cols-1 gap-3 md:grid-cols-3 xl:grid-cols-5">
+      <div className="grid grid-cols-1 gap-3 md:grid-cols-3 xl:grid-cols-5">
         <ContainerFilter />
         <div className="col-span-2 lg:col-span-2 xl:col-span-4 flex flex-col gap-12">
-            <div className="grid grid-cols-2 gap-3 gap-y-10 xl:grid-col-4">
-                    {preparedProducts.map(product => (
-                                            <CardProduct 
-                                                key={product.id}
-                                                name={product.name}
-                                                price={product.price}
-                                                colors={product.colors}
-                                                img={product.images[0]} 
-                                                slug={product.slug}
-                                                variants={product.variants}/>
-                    ))}
-            </div>
-            {/** Todo Paginacion */}
+          <div className="grid grid-cols-2 gap-3 gap-y-10 xl:grid-col-4">
+            {preparedProducts.map((product) => (
+              <CardProduct
+                key={product.id}
+                name={product.name}
+                price={product.price}
+                colors={product.colors}
+                img={product.images[0]}
+                slug={product.slug}
+                variants={product.variants}
+              />
+            ))}
+          </div>
+          {/** Todo Paginacion prueba commit*/}
         </div>
-    </div>
+      </div>
     </>
-}
+  );
+};
